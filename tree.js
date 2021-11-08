7c15e209959dbb1ff0016e078ff297fa9e73763b
jburgoon1 <77408299+jburgoon1@users.noreply.github.com>
2021-11-08 12:53:32 -0700

    finished

---
+++ tree.js
@@ -0,0 +1,76 @@
+/** TreeNode: node for a general tree. */
+
+class TreeNode {
+  constructor(val, children = []) {
+    this.val = val;
+    this.children = children;
+  }
+}
+
+class Tree {
+  constructor(root = null) {
+    this.root = root;
+  }
+
+  /** sumValues(): add up all of the values in the tree. */
+
+  sumValues() {
+    const toVist = [this.root]
+    let sum = 0
+    if(toVist[0]=== null ) return 0
+    while(toVist.length){
+      let current = toVist.pop()
+      sum += current.val
+        if(current.children){
+          for(let child of current.children){
+            toVist.push(child)
+          }
+        }
+  }
+  return sum
+  }
+
+  /** countEvens(): count all of the nodes in the tree with even values. */
+
+  countEvens() {
+    const toVist = [this.root]
+    let count = 0
+    if(toVist[0]=== null ) return 0
+    while(toVist.length){
+      let current = toVist.pop()
+      if(current.val %2===0){
+      count++
+      }
+        if(current.children){
+          for(let child of current.children){
+            toVist.push(child)
+          }
+        }
+  }
+  return count
+
+  }
+
+  /** numGreater(lowerBound): return a count of the number of nodes
+   * whose value is greater than lowerBound. */
+
+  numGreater(lowerBound) {
+    const toVist = [this.root]
+    let count = 0
+    if(toVist[0]=== null ) return 0
+    while(toVist.length){
+      let current = toVist.pop()
+      if(current.val > lowerBound){
+      count++
+      }
+        if(current.children){
+          for(let child of current.children){
+            toVist.push(child)
+          }
+        }
+  }
+  return count
+  }
+}
+
+module.exports = { Tree, TreeNode };
